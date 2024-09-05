73dc394ff7d09003404f92e3bba59f5d878fea2c
hamza1851 <hamza.dev011@gmail.com>
2024-09-05 22:31:39 +0530

    Add Solution 3

--- solutions.js
+++ solutions.js
@@ -1,29 +1,67 @@
 // const inventory = require("./inventory.js")
 
+// ----------------Solution 1----------------
+
 function findCarById(inventory, id) {
   try {
-    if (!inventory) {
-      console.log("No inventory")
-      throw new Error()
-    } else if (!(inventory.constructor === Array)) {
-      console.log("Send data in the required format")
-      throw new Error()
-    } else {
-      for (let i = 0; i < inventory.length; i++) {
-        if (inventory[i].id === id) {
-          return inventory[i]
-        }
+    if (!inventory) throw new Error("No inventory")
+
+    if (!(inventory.constructor === Array)) throw new Error("Wrong data format")
+
+    if (inventory.length === 0) throw new Error("Inventory is empty")
+
+    for (let i = 0; i < inventory.length; i++) {
+      // console.log(inventory[i].id)
+      if (inventory[i].id === id) {
+        return inventory[i]
       }
-      return null // Car not found
     }
+    return null // Car not found
   } catch (error) {
     console.log(error.message)
   }
 }
 
+// ----------------Solution 2----------------
+
 function getLastCar(inventory) {
-  if (!inventory) console.log("No inventory data exist")
+  if (!inventory) throw new Error("No inventory data exist")
+
+  if (!Array.isArray(inventory)) throw new Error("Wrong data format")
+
+  if (inventory.length === 0) throw new Error("Inventory is empty")
+
   return inventory[inventory.length - 1]
 }
 
-module.exports = { findCarById, getLastCar }
+// ----------------Solution 3----------------
+
+function sortCarModels(inventory) {
+  if (!inventory) throw new Error("No inventory data exist")
+
+  if (!Array.isArray(inventory)) throw new Error("Wrong data format")
+
+  if (inventory.length === 0) throw new Error("Inventory is empty")
+
+  const models = []
+  for (let i = 0; i < inventory.length; i++) {
+    if (inventory[i].hasOwnProperty("car_model"))
+      models.push(inventory[i].car_model)
+  }
+
+  if (models.length === 0) throw new Error("Car Model does not exist")
+
+  for (let i = 0; i < models.length - 1; i++) {
+    for (let j = i + 1; j < models.length; j++) {
+      if (models[i] > models[j]) {
+        const temp = models[i]
+        models[i] = models[j]
+        models[j] = temp
+      }
+    }
+  }
+
+  return models
+}
+
+module.exports = { findCarById, getLastCar, sortCarModels }
--- test-solutions.js
+++ test-solutions.js
@@ -1,5 +1,5 @@
 const inventory = require("./inventory.js")
-const { findCarById, getLastCar } = require("./solutions.js")
+const { findCarById, getLastCar, sortCarModels } = require("./solutions.js")
 
 // ----------------Questions 1----------------
 const car33 = findCarById(inventory, 33)
@@ -7,6 +7,10 @@ console.log(
   `Car 33 is a ${car33.car_year} ${car33.car_make} ${car33.car_model}`
 )
 
-// ----------------Questions 1----------------
+// ----------------Questions 2----------------
 const lastCar = getLastCar(inventory)
 console.log(`Last car is a ${lastCar.car_make} ${lastCar.car_model}`)
+
+// ----------------Questions 3----------------
+const sortedCarModels = sortCarModels(inventory)
+console.log(sortedCarModels)
